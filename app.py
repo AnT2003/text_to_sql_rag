3e48af15a6765dbeb7826eac9919947e6f4525a3
Thái Bảo An <113125662+AnT2003@users.noreply.github.com>
2026-01-31 23:27:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 OLLAMA_HOST = "https://ollama.com"
-MODEL_NAME = "gemini-3-pro-preview:latest"
+MODEL_NAME = "gemini-3-flash-preview:cloud"
 DEFAULT_API_KEY = os.getenv("OLLAMA_API_KEY")
 SCHEMA_FOLDER = "./schemas"
 
@@ -379,6 +379,7 @@ if __name__ == '__main__':
     init_db()
     rag_engine.load_schemas()
     app.run(debug=True, port=5000)
+
 
 
 
